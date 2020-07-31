e1e11f6eac3b3da96b52fcdaae991d68f5e72954
QuantumlyTangled <ethoarx@gmail.com>
2020-07-31 07:19:44 +0200

    feat: language directory walking

--- src/lib/util/LanguageHandler.js
+++ src/lib/util/LanguageHandler.js
@@ -1,3 +1,6 @@
+const { promises: fsp } = require("fs");
+const path = require("path");
+
 class LanguageHandler {
 
 	constructor(client) {
@@ -7,6 +10,43 @@ class LanguageHandler {
 		 * @type {AnteiClient}
 		 */
 		this.client = client;
+
+		this.directory = path.resolve(this.client.userBaseDirectory, "languages");
+
+		this.options = {
+			jsonIndent: 2,
+			loadPath: path.resolve(this.directory, path.sep, "{{lng}}/{{ns}}.json")
+		};
+	}
+
+	/**
+	 * @param {string} dir TODO
+	 * @param {string} namespaces TODO
+	 * @param {string[]} folderName TODO
+	 * @since 0.0.1
+	 * @copyright 2020 TypicalBot LLC, Nicholas Sylke and the TypicalBot contributors
+	 * @license Apache-2.0
+	 */
+	async walkLanguageDirectory(dir, namespaces, folderName) {
+		const files = await fsp.readdir(dir);
+
+		const languages = [];
+		for (const file of files) {
+			const stat = await fsp.stat(path.join(dir, file));
+			if (stat.isDirectory()) {
+				const isLanguage = file.includes("-");
+				if (isLanguage) languages.push(file);
+
+				const folder = await this.walkLanguageDirectory(path.join(dir, file), namespaces, isLanguage ? "" : `${file}/`);
+
+				// eslint-disable-next-line prefer-destructuring
+				namespaces = folder.namespaces;
+			} else {
+				namespaces.push(`${folderName}${file.substr(0, file.length - 5)}`);
+			}
+		}
+
+		return { namespaces: [...new Set(namespaces)], languages };
 	}
 
 }
