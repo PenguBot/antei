afe71ac4d39098086acf840abaacb8ce84922d24
QuantumlyTangled <ethoarx@gmail.com>
2020-08-02 17:30:53 +0200

    feat: tests base

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export * from "./lib/structures/client/BaseClient";
 export * from "./lib/structures/client/Client";
-export * from "./lib/types/ClientOptions";
+export type { ClientOptions, BaseClientOptions } from "./lib/types/ClientOptions";
+
+export const main = () => "this builds and pushes";
