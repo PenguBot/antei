b060e03050f0fa1b803284493ab31781f095b16f
QuantumlyTangled <ethoarx@gmail.com>
2020-08-02 16:18:33 +0200

    refactor: initial

--- src/events/argumentError.js
+++
@@ -1,9 +0,0 @@
-const { Event } = require("@pengubot/antei");
-
-module.exports = class extends Event {
-
-	run(message, command, params, error) {
-		message.sendMessage(error).catch(err => this.client.emit("wtf", err));
-	}
-
-};
