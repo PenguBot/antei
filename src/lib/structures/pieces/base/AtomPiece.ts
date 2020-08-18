696fa2e1de73c0772dfa56219489aeb03e2ffc04
QuantumlyTangled <ethoarx@gmail.com>
2020-08-18 22:14:48 +0200

    feat: base atom class

---
+++ src/lib/structures/pieces/base/AtomPiece.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { Piece, PieceOptions } from "./Piece";
+import { Store } from "./Store";
+
+export class AtomPiece {
+
+	public readonly store!: Store<Piece>;
+	public readonly directory!: string;
+	public readonly file!: readonly string[];
+	public readonly options!: PieceOptions;
+
+	public constructor(store: Store<Piece>, directory: string, file: readonly string[], options: PieceOptions = {}) {
+		this.store = store;
+		this.directory = directory;
+		this.file = file;
+		this.options = options;
+	}
+
+}
